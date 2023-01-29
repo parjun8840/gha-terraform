a0b5142e4922535618317c3f76a62a16a24fa00e
parjun8840 <mailbox.aadya@gmail.com>
2023-01-29 10:34:10 +0900

    modifying PR slack notifications

---
+++ scripts/jira.py
@@ -0,0 +1,26 @@
+def create_ticket():
+	try:
+		jira_connection = JIRA(
+		basic_auth=(os.environ['JIRA_EMAIL'], os.environ['JIRA_TOKEN']),
+		server=os.environ['JIRA_SERVER_URL']
+		)
+
+		issue_dict = {
+			'project': {'key': 'pixie'},
+			'summary': os.environ['JIRA_SUMMARY'],
+			'description': os.environ['JIRA_DESCRIPTION'],
+			'issuetype': {'name': 'Task'},
+			'Team': ['EKS'],
+			'Subteam': ['OnCall']
+
+			}
+
+		new_issue = jira_connection.create_issue(fields=issue_dict)
+		print(f'{new_issue.key})
+		return (f'{new_issue.key})
+	except Exception as e:
+		printf(f'Error while creating Jira Ticket: {e}')
+		return ''
+if __name__ == '__main__':
+	create_ticket()
+
